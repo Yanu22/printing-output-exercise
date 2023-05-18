1ef2a0694a9f1d0408a03fccea01b0fba6c06e9d
Yanu22 <132909605+Yanu22@users.noreply.github.com>
2023-05-18 12:49:09 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -22,6 +22,9 @@ let output = `
     a == b = ${a == b}
     a != b = ${a != b}
     a > b = ${a > b}
+    a < b = ${a < b}
+    !a && !c = ${!a && !c}
+    !a || !c = ${!a || !c}
     `;
 alert(output);
 
@@ -43,3 +46,29 @@ a < b
 !a && !c 
 !a || !c 
 */
+
+// TASK 2
+// Declare variable first_name and assign it with value "Yashvi".
+let first_name = "Yashvi";
+// Declare variable last_name and assign it with value "Panchal".
+let last_name = "Panchal";
+// Declare variable email_id and assign it with email.
+let email_id = "panc0063@algonquinlive.com";
+
+// Concatenate first_name, last_name, email_id and string literals needed 
+// to form the sentence:
+	// My name is Yashvi Panchal 
+	// you can contact me at panc0063@algonquinlive.com
+	
+    output = " My name is " + first_name + " " + last_name + " you can contact me at " + email_id + " .";
+    
+    // Assign variable experiment with concatenated expression.
+            
+    // Print experiment using alert()
+    // Print experiment using console.log()
+    console.log(output);
+    
+    // Assign variable experiment with concatenated expression.
+    `My name is ${first_name} ${last_name} you can contact me at ${email_id}.`;
+    console.log(output);
+    alert(output);
